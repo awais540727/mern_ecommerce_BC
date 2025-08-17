5ff24c9ae65377c7d5dfe1f8703fa4063aef9593
Awais Ali <awaisali786378@gmail.com>
2025-08-17 23:22:09 +0500

    Update server.js

--- server.js
+++ server.js
@@ -77,9 +77,9 @@ app.use(express.json());
 //   })
 // );
 
-// // Serve static files from the build directory
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
-// app.use(express.static(path.join(__dirname, "ecom/build")));
+// Serve static files from the build directory
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, "./ecom/build")));
 
 // Define routes
 // https://mern-ecommerce-bc.vercel.app/api/v1/auth/login
@@ -87,10 +87,10 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-// // // For any other route, serve the index.html file
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "ecom/build/index.html"));
-// });
+// For any other route, serve the index.html file
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "ecom/build/index.html"));
+});
 
 // Start the server
 const PORT = process.env.PORT || 8080;
